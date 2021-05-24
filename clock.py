63c25d8d0fbfa49ad61b635784988c587cb940f0
Mirror <jess2529156651@gmail.com>
2021-05-24 16:49:30 +0800

    feat: add notify time

--- clock.py
+++ clock.py
@@ -1,10 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import moodle_notify
+import os
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', hour=8)
+@sched.scheduled_job('cron', hour=(int(os.environ.get("NOTIFY_TIME"))+16) % 24)
 def timed_job():
     moodle_notify.moodle_notify()
 
